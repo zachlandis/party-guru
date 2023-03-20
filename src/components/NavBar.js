840ff0d24e44619e4c64fbcefd939c8f3a7d27ef
Zach <zachlandis91@gmail.com>
2023-03-20 11:59:09 -0600

    Build out NavBar component with 'Link'

--- src/components/NavBar.js
+++ src/components/NavBar.js
@@ -1,8 +1,15 @@
 import React from "react";
+import { Link } from "react-router-dom"
+import '../index.css';
 
-function NavBar () {
-
+function NavBar() {
+    return (
+        <nav className="Header">
+            <Link to="/" id="white-text">Home</Link> 
+            <Link to="/about" id="white-text" >About</Link> 
+            <Link to="/events"id="white-text" >Events</Link> 
+        </nav> 
+    )
 }
-
 
 export default NavBar
