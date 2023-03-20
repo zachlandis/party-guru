8200a980de16cced4987015d9c0389d6dacdbe15
Zach <zachlandis91@gmail.com>
2023-03-20 12:01:45 -0600

    Build out Home component

--- src/components/Home.js
+++ src/components/Home.js
@@ -1,9 +1,20 @@
 import React from "react";
+import '../index.css';
 
 function Home() {
+    
+    const pgp_logo = "https://www.party-guru.com/wp-content/uploads/2021/08/PGP_Logo_Website2019-300x97-1.png"
+
+    const party_sloth = "https://trello.com/1/cards/641146a800f511db588dc9c6/attachments/641146b4685d9fccd3cb0c21/previews/641146b6685d9fccd3cb0e1c/download/version%3D1%26uuid%3DFB13454A-6FB1-4936-8532-D1549D68F473%26mode%3Dcompatible%26noloc%3D0.png"
 
     return (
-        <h1>Home Page!</h1>
+        <div id="pgp_logo" >
+            <img src={pgp_logo} alt="pgp logo" />
+            <h6 id="white-text">Partying is the business, and business is good!</h6>
+            <br/>
+            <img src={party_sloth} alt="party sloth" id="party_sloth" />
+
+        </div>
     )
 }
 
