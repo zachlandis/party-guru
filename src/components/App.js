4fddcda304a1414fc24f75ba3df34b7ad3dfc6c5
Zach <zachlandis91@gmail.com>
2023-03-20 11:58:12 -0600

    set NavBar Routes on App

--- src/components/App.js
+++ src/components/App.js
@@ -1,4 +1,4 @@
-import React from 'react';
+import React, { useState } from 'react';
 import {Routes, Route} from "react-router-dom"
 
 import NavBar from './NavBar';
@@ -8,10 +8,21 @@ import Events from './Events';
 
 
 function App() {
+  const [page, setPage] = useState("/")
+
   return (
     <div className="App">
-      
-        
+      <NavBar onChangePage={setPage}/>
+        <Routes>
+        <Route path="/about" element={<About/>}>
+        </Route>
+        <Route exact path="/events" element={<Events/>}>
+        </Route>
+        <Route exact path="/" element={<Home/>}>
+        </Route>
+        <Route path="*" element={<h1>404 not found</h1>}>
+        </Route>
+      </Routes>
     </div>
   );
 }
