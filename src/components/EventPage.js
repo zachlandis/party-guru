92a105d3c6fc7c2d996bd74c452103099b720ead
Zach <zachlandis91@gmail.com>
2023-03-20 12:33:50 -0600

    Build out EventPage component

--- src/components/EventPage.js
+++ src/components/EventPage.js
@@ -1,8 +1,37 @@
-import React from "react"
+import React, { useEffect, useState } from "react";
+import { useParams } from "react-router-dom"
+import '../index.css';
+
 
 function EventPage() {
-    
-}
+    const [eventPage, setEventPage] = useState([])
+    const { id } = useParams()
 
+    const {eventName, eventDate, eventVenue, ticketLink, posterBanner, eventDescription} = eventPage
+
+    useEffect(() => {
+        fetch(`http://localhost:3001/events/${id}`)
+            .then(r => r.json())
+            .then(data => setEventPage(data))
+    }, [id])
+
+    if(!eventPage) return <h2 id="white-text">Loading...</h2>    
+
+    return (
+        <div className="event-page-div">
+            <div className="event-page-text">
+                <h1>{eventName}</h1>
+                <h2>{eventVenue}</h2>
+                <h3>{eventDate}</h3>
+                <img className="event-poster" src={posterBanner} alt="poster" />
+            </div>
+            <br/>
+            <p id="event-description" >{eventDescription}</p>
+            <br/>
+            <a  id="ticket-link" href={ticketLink}>Buy Tickets</a>
+        </div>
+
+    )
+}
 
 export default EventPage
